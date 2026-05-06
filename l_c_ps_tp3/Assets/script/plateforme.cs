5db5df1c5b3d4be014a14670d2fd3ab46003d794
ElChavo00 <142917881+ElChavo00@users.noreply.github.com>
2026-05-06 09:06:12 -0400

    1

--- l_c_ps_tp3/Assets/script/plateforme.cs
+++ l_c_ps_tp3/Assets/script/plateforme.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class plateforme : MonoBehaviour
 {
+    [SerializeField] GameObject pointA;
+    [SerializeField] GameObject pointB;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float delay = 1f;
+    [SerializeField] GameObject plateforme_bouge;
+
+    private Vector3 targetPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        platforme.transform.position = pointA.transform.position;
+    targetPosition: pointB.transform.position;
+    StartCoroutine(MovePlatform());
     }
 
     // Update is called once per frame
